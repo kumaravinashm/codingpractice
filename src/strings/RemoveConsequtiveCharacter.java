daa11d10d94bdfb6b027904c7cea1e4b6bd0541e
kavinash <kavinash@informatica.com>
2023-08-11 11:44:24 +0530

    dding all problems

--- src/strings/RemoveConsequtiveCharacter.java
+++ src/strings/RemoveConsequtiveCharacter.java
@@ -2,20 +2,17 @@ package strings;
 
 public class RemoveConsequtiveCharacter {
     public static void main(String[] args) {
-        String s = "ddddfffgggrrryyyuujjjkkoh";
+        String s = "dgfffgggrrryyyuujjjkkoh";
         System.out.println(removeChar(s));
     }
 
 
     static String removeChar(String s){
         int n = s.length();
-        char comp=s.charAt(0);
         String result = String.valueOf(s.charAt(0));
-        for(int i=1;i<n-1;i++){
-            if(s.charAt(i+1)!=comp){
+        for(int i=0;i<n-1;i++){
+            if(s.charAt(i)!=s.charAt(i+1)){
                 result = result+s.charAt(i+1);
-                comp = s.charAt(i+1);
-
             }
         }
         return result;
